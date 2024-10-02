5c4163efb107aaebf985edfb4da218d639fa6c0c
armanabbas <armanabbas2000@gmail.com>
2024-10-02 09:01:47 -0700

    Add files via upload

---
+++ src/app/components/header.tsx
@@ -0,0 +1,13 @@
+export default function Header(){
+return(
+    <div className="headerfirst">
+      <div className="headersecond">
+        <ul className="headerbutton"> 
+        <li> Home </li> 
+        <li> contact Me </li> 
+        <li> protolio </li>
+        </ul>
+      </div>
+    </div>
+);
+}
---
+++ src/app/page.tsx
@@ -0,0 +1,15 @@
+import Header from "@/app/components/header";
+
+export default function Home() {
+    return(
+        <div className="firstcontainer">
+            <header />
+            <div className="body">
+                hello everyone.
+                <br />
+                this is home page.
+                </div>
+               
+        </div>
+    );
+}
